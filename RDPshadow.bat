a44f38d35f7bf5ae4e3f61b8d0748114ff80c334
AlektroNik <alektro@ya.ru>
2021-07-06 14:19:29 +0300

    Revert "Удалил запрос Логина/пароля "
    This reverts commit ac21cc8c284ae857b4ee5cc7d91fa0ae7039f6a1.

--- RDPshadow.bat
+++ RDPshadow.bat
@@ -2,4 +2,4 @@
 set /P rcomp="Enter name or IP of a Remote PC: "
 query session /server:%rcomp%
 set /P rid="Enter RDP user ID: "
-start mstsc /shadow:%rid% /v:%rcomp% /control /noConsentPrompt
+start mstsc /shadow:%rid% /v:%rcomp% /control /noConsentPrompt /prompt
